ca11f62bf3a192044c549f3f029fa9837522078f
unknown <bati.mayer@gmail.com>
2023-07-11 00:31:23 +0300

    added get req for registered-events-ids

--- api/events.js
+++ api/events.js
@@ -9,31 +9,53 @@ const { eventValidationSchema } = require('../db/validationSchema')
 router.use(cors())
 
 
-router.post('/registered-events', async (req, res, next) => {
-    const eventsId = req.body
-    console.log(eventsId)
 
+router.get('/registered-events-ids', async (req, res) => {
     try {
+        const userId = req.user
+        const user = await User.findOne({ _id: userId })
+        if (!user) {
+            return res.status(404).json({ success: false, data: null, error: 'user not found' })
+        }
+
+        const eventsId = user.registeredEvents
+        res.status(200).json({ success: true, data: eventsId, error: null })
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({ success: false, data: null, error: 'internal server error' })
+    }
+})
+
+
+
+router.get('/registered-events', async (req, res) => {
+    try {
+        const userId = req.user
+        const user = await User.findOne({ _id: userId })
+        if (!user) {
+            return res.status(404).json({ success: false, data: null, error: 'user not found' })
+        }
+
+        const eventsId = user.registeredEvents
         const events = await Event.find({ _id: { $in: eventsId } })
-        if (!events || events.length === 0) {
+        if (events.length === 0) {
             return res.status(404).json({ success: false, data: null, error: 'No events found' })
         }
 
-        const restructuredData = events.map(({ title, shortDesc, date, _id, image }) => ({
+        const restructuredData = events.map(({ _id, title, shortDesc, date, image }) => ({
             id: _id,
             title,
             shortDesc,
             image: `http://localhost:999/public/uploads/${image}`,
             date
         }))
-        console.log(restructuredData)
+
         res.status(200).json({ success: true, data: restructuredData, error: null })
     } catch (error) {
-        console.log(error.message)
-        res.status(500).json({ success: false, data: null, error: error })
+        console.error(error.message)
+        res.status(500).json({ success: false, data: null, error: 'Internal server error' })
     }
 })
-
 
 
 router.delete('/:id/register', async (req, res, next) => {
@@ -41,35 +63,47 @@ router.delete('/:id/register', async (req, res, next) => {
     const userId = req.user
 
 
+
     if (!userId || !eventId) {
-        return res.status(400).json({ success: false,data:null, error: 'invalid userId or eventId' })
+        return res.status(400).json({ success: false, data: null, error: 'invalid userId or eventId' })
     }
 
     try {
-        const user = await User.findById(userId)
+        const user = await User.findOne({ _id: userId })
+
+
         if (!user) {
-            return res.status(404).json({ success: false, data:null, error: 'user not found' })
+            return res.status(404).json({ success: false, data: null, error: 'user not found' })
         }
 
         const event = await Event.findById(eventId)
+
+
         if (!event) {
             return res.status(404).json({ success: false, data: null, error: 'event not found' })
         }
 
         if (user.registeredEvents.includes(eventId)) {
             user.registeredEvents.pull(eventId)
+        } else {
+            return res.status(400).json({ success: false, data: null, error: 'user not registered' })
         }
 
 
         if (event.registeredUsers.includes(userId)) {
             event.registeredUsers.pull(userId)
+        } else {
+            return res.status(400).json({ success: false, data: null, error: 'user not registered' })
         }
 
 
         await user.save()
         await event.save()
 
-        res.status(200).json({ success: true, data: { message: 'User unregistered successfully from the event' }, error: null })
+        const userRegisteredEvents = user.registeredEvent
+
+
+        res.status(200).json({ success: true, data: userRegisteredEvents, error: null })
     } catch (error) {
         console.log(error)
         res.status(500).json({ success: false, data: null, error: 'Internal server error' })
@@ -88,7 +122,8 @@ router.patch('/:id/register', async (req, res, next) => {
     }
 
     try {
-        const user = await User.findById(userId)
+        const user = await User.findOne({ _id: userId })
+
         if (!user) {
             return res.status(404).json({ success: false, error: 'user not found' })
         }
@@ -99,17 +134,18 @@ router.patch('/:id/register', async (req, res, next) => {
 
 
         const event = await Event.findById(eventId)
+
         if (!event) {
             return res.status(404).json({ success: false, error: 'event not found' })
         }
 
         // Check if the event date has not passed
-        const currentDate = new Date()
-        const eventDate = new Date(event.date)
+        // const currentDate = new Date()
+        // const eventDate = new Date(event.date)
 
-        if (eventDate < currentDate) {
-            return res.status(400).json({ success: false, error: 'Event date has passed' })
-        }
+        // if (eventDate < currentDate) {
+        //     return res.status(400).json({ success: false, error: 'event date has passed' })
+        // }
 
 
         if (event.registeredUsers.includes(userId)) {
@@ -122,7 +158,9 @@ router.patch('/:id/register', async (req, res, next) => {
         await user.save()
         await event.save()
 
-        res.status(200).json({ success: true, data: { message: 'user registered successfully to event' }, error: null })
+        const userRegisteredEvents = user.registeredEvents
+
+        res.status(200).json({ success: true, data: userRegisteredEvents, error: null })
 
     } catch (error) {
         console.log(error)
@@ -177,7 +215,7 @@ router.post('/', async (req, res, next) => {
                     data: newEvent,
                     error: null
                 })
-                console.log(newEvent)
+
             })
         } catch (err) {
             console.log(err)
@@ -190,37 +228,59 @@ router.post('/', async (req, res, next) => {
 
 
 router.get('/:id', async (req, res, next) => {
+    const eventId = req.params.id
+    const userId = req.user
+
+    if (!userId || !eventId) {
+        return res.status(400).json({ success: false, data: null, error: 'Invalid userId or eventId' })
+    }
 
     try {
-        const event = await Event.findOne({ _id: req.params.id })
+        const event = await Event.findOne({ _id: eventId })
         if (!event) {
-            return res.status(404).json({ success: false, data: null, error: 'event not found' })
-        } else {
-            const restructuredEventData = {
-                id: event._id,
-                title: event.title,
-                shortDesc: event.shortDesc,
-                longDesc: event.longDesc,
-                date: event.date,
-                time: event.time,
-                location: event.location,
-                host: event.host,
-                hostDesc: event.hostDesc,
-                tags: event.tags,
-                image: `http://localhost:999/public/uploads/${event.image}`,
-                alt: event.alt
-            }
-            res.status(200).json({ success: true, data: restructuredEventData, error: null })
-
+            return res.status(404).json({ success: false, data: null, error: 'Event not found' })
         }
 
+        const user = await User.findOne({ _id: userId })
+        if (!user) {
+            return res.status(404).json({ success: false, data: null, error: 'User not found' })
+        }
 
+        // const isRegistered = user.registeredEvents.includes(eventId)
 
+        const restructuredEventData = {
+            id: event._id,
+            title: event.title,
+            shortDesc: event.shortDesc,
+            longDesc: event.longDesc,
+            date: event.date,
+            time: event.time,
+            location: event.location,
+            host: event.host,
+            hostDesc: event.hostDesc,
+            tags: event.tags,
+            image: `http://localhost:999/public/uploads/${event.image}`,
+            alt: event.alt,
+            // isRegistered: isRegistered
+        }
+
+        const userRegisteredEvents = user.registeredEvents
+
+        res.status(200).json({
+            success: true,
+            data: {
+                event: restructuredEventData,
+                userRegisteredEvents: userRegisteredEvents
+            },
+            error: null
+        })
     } catch (err) {
         console.log(err)
         res.status(500).json({ success: false, data: null, error: 'Internal server error' })
     }
 })
+
+
 
 router.get('/', async (req, res, next) => {
     try {
@@ -236,7 +296,7 @@ router.get('/', async (req, res, next) => {
             image: `http://localhost:999/public/uploads/${image}`,
             date
         }))
-        console.log(restructuredData)
+
         res.status(200).json({ success: true, data: restructuredData, error: null })
     } catch (error) {
         res.status(500).json({ success: false, data: null, error: 'Internal server error' })
